9123c83dacc5a7bcef7319233503979ef3340021
Jolas <jolas.l@zohocorp.com>
2020-05-20 23:43:12 +0530

    build.gradle updated to add SQLDelight dependency

--- src/commonMain/kotlin/com.jolas.sdk.kn.newsycore/DataModels.kt
+++ src/commonMain/kotlin/com.jolas.sdk.kn.newsycore/DataModels.kt
@@ -1,8 +1,7 @@
 package com.jolas.sdk.kn.newsycore
 
-import kotlinx.serialization.*
-import kotlinx.serialization.builtins.ArraySerializer
-import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 @Serializable
 enum class ItemType(val value: String){
@@ -64,118 +63,76 @@ data class User(
 data class Item(
     val id: Long,
     @SerialName("deleted")
-    val isDeleted: Boolean = false, //Needs mapping
+    val isDeleted: Boolean = false,
     @SerialName("type")
-    val itemType: ItemType = ItemType.STORY, //Needs mapping
+    val itemType: ItemType = ItemType.STORY,
     val by: String? = null,
     val time: Long,
     val text: String? = null,
     @SerialName("dead")
-    val isDead: Boolean = false, //Needs mapping
+    val isDead: Boolean = false,
     @SerialName("parent")
-    val parentItemId: Long? = null, //Needs mapping
+    val parentItemId: Long? = null,
     val poll: Long? = null,
     val kids: Array<Long>? = null,
     @SerialName("url")
-    val urlString: String? = null, //Needs mapping
+    val urlString: String? = null,
     val score: Int? = null,
     val title: String? = null,
     @SerialName("parts")
     val parts: Array<Int>? = null,
     val descendants: Int? = null,
     val lastRefreshTime: Int? = null
-)/* {
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
 
-    @InternalSerializationApi
-    @ImplicitReflectionSerializer
-    @Serializer(forClass = Item::class)
-    companion object : KSerializer<Item> {
-        override val descriptor: SerialDescriptor = SerialDescriptor("Item"){
-            element<Long>("id")
-            element<Boolean>("deleted")
-            element<String>("type")
-            element<String>("by")
-            element<Long>("time")
-            element<String>("text")
-            element<Boolean>("dead")
-            element<Long>("parent")
-            element<Long>("poll")
-            element<Array<Long>>("kids")
-            element<String>("url")
-            element<Int>("score")
-            element<String>("title")
-            element<Array<Long>>("parts")
-            element<Int>("descendants")
-        }
+        other as Item
 
-        override fun deserialize(decoder: Decoder): Item {
-            var id: Long = -1
-            var isDeleted: Boolean = false
-            var type: String = ItemType.STORY.value
-            var by: String? = null
-            var time: Long = -1
-            var text: String? = null
-            var isDead: Boolean = false
-            var parentItemId: Long? = null
-            var poll: Long? = null
-            var kids: Array<Long>? = null
-            var urlString: String? = null
-            var score: Int? = null
-            var title: String? = null
-            var parts: Array<Int>? = null
-            var descendants: Int? = null
+        if (id != other.id) return false
+        if (isDeleted != other.isDeleted) return false
+        if (itemType != other.itemType) return false
+        if (by != other.by) return false
+        if (time != other.time) return false
+        if (text != other.text) return false
+        if (isDead != other.isDead) return false
+        if (parentItemId != other.parentItemId) return false
+        if (poll != other.poll) return false
+        if (kids != null) {
+            if (other.kids == null) return false
+            if (!kids.contentEquals(other.kids)) return false
+        } else if (other.kids != null) return false
+        if (urlString != other.urlString) return false
+        if (score != other.score) return false
+        if (title != other.title) return false
+        if (parts != null) {
+            if (other.parts == null) return false
+            if (!parts.contentEquals(other.parts)) return false
+        } else if (other.parts != null) return false
+        if (descendants != other.descendants) return false
+        if (lastRefreshTime != other.lastRefreshTime) return false
 
-            val itemDecoder = decoder.beginStructure(descriptor = descriptor)
-            loop@ while (true){
-                when (val i = itemDecoder.decodeElementIndex(descriptor)) {
-                    CompositeDecoder.READ_DONE -> break@loop
-                    0 -> id = itemDecoder.decodeLongElement(descriptor, i)
-                    1 -> isDeleted = itemDecoder.decodeBooleanElement(descriptor, i)
-                    2 -> type = itemDecoder.decodeStringElement(descriptor, i)
-                    3 -> by = null
-                    4 -> time = itemDecoder.decodeLongElement(descriptor, i)
-                    5 -> text = itemDecoder.decodeStringElement(descriptor, i)
-                    6 -> isDead = itemDecoder.decodeBooleanElement(descriptor, i)
-                    7 -> parentItemId = itemDecoder.decodeLongElement(descriptor, i)
-                    8 -> poll = itemDecoder.decodeLongElement(descriptor, i)
-                    9 -> kids = itemDecoder.decodeSerializableElement(descriptor, i, ArraySerializer(Long.serializer()))
-                    10 -> urlString = itemDecoder.decodeStringElement(descriptor, i)
-                    11 -> score = itemDecoder.decodeIntElement(descriptor, i)
-                    12 -> title = itemDecoder.decodeStringElement(descriptor, i)
-                    13 -> parts = itemDecoder.decodeSerializableElement(descriptor, i, ArraySerializer(Int.serializer()))
-                    14 -> descendants = itemDecoder.decodeIntElement(descriptor, i)
-                    else -> throw SerializationException("Unknown index $i")
-                }
-            }
-            itemDecoder.endStructure(descriptor)
-
-            val itemType = when (type){
-                "story" -> ItemType.STORY
-                "job" -> ItemType.JOB
-                "comment" -> ItemType.COMMENT
-                "poll" -> ItemType.POLL
-                "pollopt" -> ItemType.POLLOPT
-                else -> ItemType.STORY
-            }
-
-            return Item(
-                id = id,
-                isDeleted = isDeleted,
-                itemType = itemType,
-                by = by,
-                time = time,
-                text = text,
-                isDead = isDead,
-                parentItemId = parentItemId,
-                poll = poll,
-                kids = kids,
-                urlString = urlString,
-                score = score,
-                title = title,
-                parts = parts,
-                descendants = descendants,
-                lastRefreshTime = 0
-            )
-        }
+        return true
     }
-}*/
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + isDeleted.hashCode()
+        result = 31 * result + itemType.hashCode()
+        result = 31 * result + (by?.hashCode() ?: 0)
+        result = 31 * result + time.hashCode()
+        result = 31 * result + (text?.hashCode() ?: 0)
+        result = 31 * result + isDead.hashCode()
+        result = 31 * result + (parentItemId?.hashCode() ?: 0)
+        result = 31 * result + (poll?.hashCode() ?: 0)
+        result = 31 * result + (kids?.contentHashCode() ?: 0)
+        result = 31 * result + (urlString?.hashCode() ?: 0)
+        result = 31 * result + (score ?: 0)
+        result = 31 * result + (title?.hashCode() ?: 0)
+        result = 31 * result + (parts?.contentHashCode() ?: 0)
+        result = 31 * result + (descendants ?: 0)
+        result = 31 * result + (lastRefreshTime ?: 0)
+        return result
+    }
+}
