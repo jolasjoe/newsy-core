80369bb183da27984fb23c2c2c5b2d068bb87cd7
jolasjoe <40016606+jolasjoe@users.noreply.github.com>
2021-06-13 09:17:22 +0530

    Signing and Sonatype Publishing tasks added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import java.util.*
+
 plugins {
     id("com.android.library")
     id("com.squareup.sqldelight")
     kotlin("multiplatform")version("1.4.32")
     id("maven-publish")
+    id("signing")
     id("org.jetbrains.kotlin.plugin.serialization")version("1.4.32")
     id("org.jetbrains.dokka")version("1.4.32")
 }
@@ -123,4 +126,85 @@ sqldelight {
     database("NewsyDatabase") {
         packageName = "io.jolas.newsycore"
     }
+}
+
+// Stub secrets to let the project sync and build without the publication values set up
+ext["signing.keyId"] = null
+ext["signing.password"] = null
+ext["signing.secretKeyRingFile"] = null
+ext["ossrhUsername"] = null
+ext["ossrhPassword"] = null
+
+// Grabbing secrets from local.properties file or from environment variables, which could be used on CI
+val secretPropsFile = project.rootProject.file("local.properties")
+if (secretPropsFile.exists()) {
+    secretPropsFile.reader().use {
+        Properties().apply {
+            load(it)
+        }
+    }.onEach { (name, value) ->
+        ext[name.toString()] = value
+    }
+} else {
+    ext["signing.keyId"] = System.getenv("SIGNING_KEY_ID")
+    ext["signing.password"] = System.getenv("SIGNING_PASSWORD")
+    ext["signing.secretKeyRingFile"] = System.getenv("SIGNING_SECRET_KEY_RING_FILE")
+    ext["ossrhUsername"] = System.getenv("OSSRH_USERNAME")
+    ext["ossrhPassword"] = System.getenv("OSSRH_PASSWORD")
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+fun getExtraString(name: String) = ext[name]?.toString()
+
+publishing {
+    // Configure maven central repository
+    repositories {
+        maven {
+            name = "sonatype"
+            setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = getExtraString("ossrhUsername")
+                password = getExtraString("ossrhPassword")
+            }
+        }
+    }
+
+    // Configure all publications
+    publications.withType<MavenPublication> {
+
+        // Stub javadoc.jar artifact
+        artifact(javadocJar.get())
+
+        // Provide artifacts information requited by Maven Central
+        pom {
+            name.set("Newsy Core")
+            description.set("HN API library for multiple platforms using KMP")
+            url.set("https://github.com/jolasjoe/newsy-core")
+
+            licenses {
+                license {
+                    name.set("MIT")
+                    url.set("https://opensource.org/licenses/MIT")
+                }
+            }
+            developers {
+                developer {
+                    id.set("jolasjoe")
+                    name.set("Jolas Joe")
+                    email.set("jolasjoe@gmail.com")
+                }
+            }
+            scm {
+                url.set("https://github.com/jolasjoe/newsy-core")
+            }
+
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
 }
